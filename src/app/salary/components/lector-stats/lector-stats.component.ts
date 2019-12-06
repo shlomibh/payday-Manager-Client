1928a218a133b1037d02e93741212325302a07c1
oreny <orenys7@gmail.com>
2019-12-06 11:20:57 +0200

    'chart'

--- src/app/salary/components/lector-stats/lector-stats.component.ts
+++ src/app/salary/components/lector-stats/lector-stats.component.ts
@@ -1,7 +1,5 @@
-import { Component, OnInit, Input, OnDestroy } from '@angular/core';
-import { StatsService } from 'src/app/services/stats.service';
-import { Subscription } from 'rxjs';
-
+import { Component, OnInit, Input } from '@angular/core';
+import { Chart } from 'chart.js';
 @Component({
   selector: 'app-lector-stats',
   templateUrl: './lector-stats.component.html',
@@ -10,11 +8,48 @@ import { Subscription } from 'rxjs';
 export class LectorStatsComponent implements OnInit {
   @Input() lecStat;
   @Input() index;
+  barChart;
   options = ['ביטול שיעור', 'מחלה', 'חופש', 'הגשת דוח בזמן', 'שעות נוספות'];
 
   constructor() { }
 
   ngOnInit() {
+    this.barChart = new Chart('barChart', {
+      type: 'bar',
+      data: {
+        labels: ["Red", "Blue", "Green", "Black", "Yellow", "Purple"],
+        datasets: [{
+          label: '',
+          data: [12, 19, 3, 5, 2, 3],
+          backgroundColor: [
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(255, 206, 86, 0.2)',
+            'rgba(75, 192, 192, 0.2)',
+            'rgba(153, 102, 255, 0.2)',
+            'rgba(255, 159, 64, 0.2)'
+          ],
+          borderColor: [
+            'rgba(255, 99, 132, 1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)',
+            'rgba(75, 192, 192, 1)',
+            'rgba(153, 102, 255, 1)',
+            'rgba(255, 159, 64, 1)'
+          ],
+          borderWidth: 1
+        }]
+      },
+      options: {
+        scales: {
+          yAxes: [{
+            ticks: {
+              beginAtZero: true
+            }
+          }]
+        }
+      }
+    });
   }
 
 }
